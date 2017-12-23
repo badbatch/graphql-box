e64294dc677c0f3a35b8182b966ccd3e1a445cf0
Dylan Aubrey <dylanaubrey@gmail.com>
2017-12-23 15:53:51 +0000

    Refactoring test structure.

---
+++ test/specs/server/index.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { browserArgs, serverArgs } from "../../helpers";
+import createHandl from "../../../src";
+
+describe("the handl class in 'internal' mode", () => {
+  describe("the request method", () => {
+
+  });
+});
