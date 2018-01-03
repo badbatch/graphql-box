84a954a11a4b266f1199e118b2be173c8b2aa7e0
Dylan Aubrey <dylanaubrey@gmail.com>
2018-01-03 21:45:09 +0000

    Updating test queries and responses.

--- test/mocks/index.ts
+++ test/mocks/index.ts
@@ -2,4 +2,4 @@ import { github } from "../data/graphql";
 import { mockGraphqlRequest } from "../helpers";
 
 mockGraphqlRequest(github.requests.singleQuery);
-mockGraphqlRequest(github.requests.editedSingleQuery);
+mockGraphqlRequest(github.requests.reducedSingleQuery);
