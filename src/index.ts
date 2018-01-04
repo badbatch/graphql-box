ee6ca208ff3dc6ec4c9998cab90f96a3d6ece2e3
Dylan Aubrey <dylanaubrey@gmail.com>
2018-01-04 16:18:26 +0000

    Updating build process to reduce bundle sizes.

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,7 @@
-import { isPlainObject } from "lodash";
-import Client from "./client";
 import { ClientArgs, RequestOptions, RequestResult } from "./types";
-import WorkerClient from "./worker-client";
-
-declare global {
-  interface Window {
-      Worker: Worker;
-  }
-  interface WorkerGlobalScope {
-    Worker: Worker;
-  }
-}
-
-export type Handl = Client | WorkerClient;
 export type HandlArgs = ClientArgs;
 export type HandleRequestOptions = RequestOptions;
 export type HandleRequestResult = RequestResult;
-
-export default async function createHandl(args: HandlArgs): Promise<Handl> {
-  if (!isPlainObject(args)) {
-    throw new TypeError("createHandl expected args to ba a plain object.");
-  }
-
-  let client: Handl;
-
-  if (process.env.WEB_ENV) {
-    if (self.Worker && self.indexedDB) {
-      client = await WorkerClient.create(args);
-    } else {
-      client = await Client.create(args);
-    }
-  } else {
-    client = await Client.create(args);
-  }
-
-  return client;
-}
+export { DefaultClient as DefaultHandl } from "./default-client";
+export { WorkerClient as WorkerHandl } from "./worker-client";
+export { Client as Handl } from "./client";
