13eaa7efa0106d1d3dbabc3e8219b8c76fa10838
Dylan Aubrey <dylanaubrey@gmail.com>
2018-01-06 23:43:53 +0000

    Setting up mutation mock data.

--- test/schema/index.ts
+++ test/schema/index.ts
@@ -1,4 +1,5 @@
 import { GraphQLSchema } from "graphql";
+import mutationType from "./types/mutation";
 import queryType from "./types/query";
 import inventoryType from "./types/query/inventory";
 import listingInventoryType from "./types/query/inventory/listing";
@@ -6,6 +7,7 @@ import productInventoryType from "./types/query/inventory/product";
 import skuInventoryType from "./types/query/inventory/sku";
 
 export default new GraphQLSchema({
+  mutation: mutationType,
   query: queryType,
   types: [inventoryType, productInventoryType, skuInventoryType, listingInventoryType],
 });
