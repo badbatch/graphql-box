42cd946fbaee152a8eee1700f06c9aa482afcc17
Dylan Aubrey <dylanaubrey@gmail.com>
2018-01-09 18:15:24 +0000

    Reworking websocket proxy to reduce bundle size.

--- src/web-socket-proxy/index.ts
+++ src/web-socket-proxy/index.ts
@@ -1,20 +1,27 @@
-import { isPlainObject, isString } from "lodash";
-import * as WebSocket from "ws";
+import { isString } from "lodash";
 import { InternalSubscriber } from "../types";
+
+let websocket: typeof WebSocket;
+
+if (process.env.WEB_ENV) {
+  websocket = WebSocket;
+} else {
+  websocket = require("ws");
+}
 
 export default class WebSocketProxy {
   private _address: string;
-  private _options: WebSocket.ClientOptions;
+  private _closedCode?: number;
+  private _closedReason?: string;
   private _socket: WebSocket;
   private _subscriptions: Map<string, InternalSubscriber> = new Map();
 
-  constructor(address: string, opts?: WebSocket.ClientOptions) {
+  constructor(address: string) {
     if (!isString(address)) {
       throw new TypeError("constructor expected address to be a string.");
     }
 
     this._address = address;
-    if (opts && isPlainObject(opts)) this._options = opts;
     this._open();
   }
 
@@ -23,20 +30,36 @@ export default class WebSocketProxy {
     hash: string,
     subscriber: InternalSubscriber,
   ): Promise<{ subscribed: boolean }> {
-    return new Promise((resolve: (value: { subscribed: boolean }) => void, reject: (reason: Error) => void) => {
-      this._socket.send(JSON.stringify({ subscriptionID: hash, subscription }), (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          this._subscriptions.set(hash, subscriber);
-          resolve({ subscribed: true });
-        }
-      });
-    });
+    if (this._isClosed()) {
+      return Promise.reject(new Error(`The websocket is closed. ${this._closedCode}: ${this._closedReason}`));
+    }
+
+    if (!this._isOpen()) {
+      return Promise.reject(new Error("The websocket is not open."));
+    }
+
+    this._socket.send(JSON.stringify({ subscriptionID: hash, subscription }));
+    return { subscribed: true };
   }
 
-  private _onMessage(data: string): void {
-    const parsedData = JSON.parse(data);
+  private _isClosed(): boolean {
+    return this._socket.readyState === 2 || this._socket.readyState === 3;
+  }
+
+  private _isOpen(): boolean {
+    return this._socket.readyState === 1;
+  }
+
+  private _onClose(ev: CloseEvent): void {
+    if (!ev.wasClean) {
+      this._closedCode = ev.code;
+      this._closedReason = ev.reason;
+      this._open();
+    }
+  }
+
+  private _onMessage(ev: MessageEvent): void {
+    const parsedData = JSON.parse(ev.data);
     if (!parsedData) return;
     const { result, subscriptionID } = parsedData;
     const subscriber = this._subscriptions.get(subscriptionID);
@@ -44,14 +67,15 @@ export default class WebSocketProxy {
     subscriber(result);
   }
 
-  private async _open(): Promise<void> {
-    this._socket = new WebSocket(this._address, this._options);
+  private _onOpen(): void {
+    this._closedCode = undefined;
+    this._closedReason = undefined;
+  }
 
-    return new Promise((resolve: (value: undefined) => void) => {
-      this._socket.on("open", () => {
-        this._socket.on("message", this._onMessage);
-        resolve(undefined);
-      });
-    });
+  private async _open(): Promise<void> {
+    this._socket = new websocket(this._address);
+    this._socket.onclose = this._onClose.bind(this);
+    this._socket.onmessage = this._onMessage.bind(this);
+    this._socket.onopen = this._onOpen.bind(this);
   }
 }
