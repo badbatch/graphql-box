aca3eac20e3a413002d2d696824668c8a2a2f071
Dylan Aubrey <dylan.aubrey@gmail.com>
2018-03-18 16:52:46 +0000

    Adding timer decorator to request method and upated docs.

--- src/client-handl/index.ts
+++ src/client-handl/index.ts
@@ -28,6 +28,7 @@ import hashRequest from "../helpers/hash-request";
 import { getOperationDefinitions } from "../helpers/parsing";
 import rehydrateCacheMetadata from "../helpers/rehydrate-cache-metadata";
 import socketsSupported from "../helpers/sockets-supported";
+import { clockAsyncMethod } from "../performance";
 import GraphQLExecuteProxy from "../proxies/graphql-execute";
 import GraphQLSubscribeProxy from "../proxies/graphql-subscribe";
 import { RequestParser } from "../request-parser";
@@ -351,6 +352,7 @@ export class ClientHandl {
    * handles request parsing, filtering and caching.
    *
    */
+  @clockAsyncMethod
   public async request(query: string, opts: RequestOptions = {}): Promise<RequestResult> {
     let errors: Error[] = [];
 
