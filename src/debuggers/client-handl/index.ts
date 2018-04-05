d9c2368655b1cb47958b441034c2407331773318
Dylan Aubrey <dylan.aubrey@gmail.com>
2018-04-05 22:41:29 +0100

    Fixing typescript error.

--- src/debuggers/client-handl/index.ts
+++ src/debuggers/client-handl/index.ts
@@ -2,7 +2,7 @@ import * as EventEmitter from "eventemitter3";
 import logger from "../../logger";
 import { ObjectMap } from "../../types";
 
-class HandlDebugger extends EventEmitter {
+export class HandlDebugger extends EventEmitter {
   public emit(event: string, props: ObjectMap): boolean {
     const hasListeners = super.emit(event, props);
     logger.debug(event, props);
