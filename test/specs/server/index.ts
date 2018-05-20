c12b063b88c933a49bcbf28bb63cbffda49d5053
Dylan Aubrey <dylanaubrey@gmail.com>
2018-05-20 22:08:34 +0100

    Adding type cache controls unit tests.

--- test/specs/server/index.ts
+++ test/specs/server/index.ts
@@ -1,4 +1,5 @@
 import { serverClientArgs, subscriptionClientArgs } from "../../helpers";
+import testTypeCacheControls from "./type-cache-controls";
 import testImportExportMethods from "./import-export";
 import testMutationOperation from "./mutation";
 import testQueryOperation from "./query";
@@ -8,3 +9,4 @@ testQueryOperation(serverClientArgs);
 testMutationOperation(serverClientArgs);
 testSubscriptionOperation(subscriptionClientArgs);
 testImportExportMethods(serverClientArgs);
+testTypeCacheControls(serverClientArgs);
