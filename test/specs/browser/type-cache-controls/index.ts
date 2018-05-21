78da452052a881669d7568fb55a7a457937fa55a
Dylan Aubrey <dylan.aubrey@gmail.com>
2018-05-21 23:02:26 +0100

    Finishing unit tests of type cache controls.

--- test/specs/browser/type-cache-controls/index.ts
+++ test/specs/browser/type-cache-controls/index.ts
@@ -58,7 +58,7 @@ export default function testTypeCacheControls(args: ClientArgs, opts: { suppress
         });
       });
 
-      context.only("when a type cache control is set on the Query type", () => {
+      context("when a type cache control is set on the Query type", () => {
         let result: RequestResultData;
 
         before(async () => {
@@ -86,6 +86,74 @@ export default function testTypeCacheControls(args: ClientArgs, opts: { suppress
             expect(cacheMetadata.size).to.equal(1);
             const operationCacheability = cacheMetadata.get("query") as Cacheability;
             expect(operationCacheability.metadata.cacheControl.maxAge).to.equal(60);
+        });
+      });
+
+      context("when a type cache control is set on the Organization type", () => {
+        let result: RequestResultData;
+
+        before(async () => {
+          try {
+            await client.setTypeCacheControls({ Organization: "max-age=120" });
+
+            result = await client.request(
+              github.requests.singleQuery,
+              { awaitDataCached: true, variables: { login: "facebook" } },
+            ) as RequestResultData;
+          } catch (error) {
+            console.log(error); // tslint:disable-line
+          }
+        });
+
+        after(async () => {
+          await client.setTypeCacheControls({});
+          await client.clearCache();
+          if (onMainThread) fetchMock.reset();
+        });
+
+        it(`then the cache metadata should be made up of the Query type cache control and what was received
+          from the server`, () => {
+            const cacheMetadata = result.cacheMetadata;
+            expect(cacheMetadata.size).to.equal(2);
+            const operationCacheability = cacheMetadata.get("query") as Cacheability;
+            expect(operationCacheability.metadata.cacheControl.maxAge).to.equal(120);
+            const organizationCacheability = cacheMetadata.get("organization") as Cacheability;
+            expect(organizationCacheability.metadata.cacheControl.maxAge).to.equal(120);
+        });
+      });
+
+      context("when a type cache control is set on the Organization and Repository types", () => {
+        let result: RequestResultData;
+
+        before(async () => {
+          try {
+            await client.setTypeCacheControls({ Organization: "max-age=240", Repository: "max-age=30" });
+
+            result = await client.request(
+              github.requests.singleQuery,
+              { awaitDataCached: true, variables: { login: "facebook" } },
+            ) as RequestResultData;
+          } catch (error) {
+            console.log(error); // tslint:disable-line
+          }
+        });
+
+        after(async () => {
+          await client.setTypeCacheControls({});
+          await client.clearCache();
+          if (onMainThread) fetchMock.reset();
+        });
+
+        it(`then the cache metadata should be made up of the Organization and Repository type cache controls and what
+          was received from the server`, () => {
+            const cacheMetadata = result.cacheMetadata;
+            expect(cacheMetadata.size).to.equal(3);
+            const operationCacheability = cacheMetadata.get("query") as Cacheability;
+            expect(operationCacheability.metadata.cacheControl.maxAge).to.equal(30);
+            const organizationCacheability = cacheMetadata.get("organization") as Cacheability;
+            expect(organizationCacheability.metadata.cacheControl.maxAge).to.equal(240);
+            const repositoryCacheability = cacheMetadata.get("organization.repositories.edges.node") as Cacheability;
+            expect(repositoryCacheability.metadata.cacheControl.maxAge).to.equal(30);
         });
       });
     });
