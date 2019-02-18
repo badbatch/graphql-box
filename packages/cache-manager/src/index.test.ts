bb2dffb59bb8b6561955c65663fbc9f6238f8046
Dylan Aubrey <dylanaubrey@gmail.com>
2019-02-18 22:24:23 +0000

    update test structure.

--- packages/cache-manager/src/index.test.ts
+++ packages/cache-manager/src/index.test.ts
@@ -18,67 +18,69 @@ describe("@handl/cache-manager", () => {
     let responseData: ResponseData;
     let requestData: RequestData;
 
-    describe("when caching is done through cascading cache control", () => {
-      beforeAll(async () => {
-        cacheManager = await CacheManager.init({
-          cache: await Core.init({
-            name: "cachemap",
-            store: map(),
-          }),
-          cascadeCacheControl: true,
-          typeIDKey: DEFAULT_TYPE_ID_KEY,
+    describe("a simple query", () => {
+      describe("when caching is done through cascading cache control", () => {
+        beforeAll(async () => {
+          cacheManager = await CacheManager.init({
+            cache: await Core.init({
+              name: "cachemap",
+              store: map(),
+            }),
+            cascadeCacheControl: true,
+            typeIDKey: DEFAULT_TYPE_ID_KEY,
+          });
+
+          requestData = getRequestData(githubParsedQueries.simple);
+
+          responseData = await cacheManager.resolveQuery(
+            requestData,
+            requestData,
+            githubQueryResponses.simple,
+            { awaitDataCaching: true },
+            getRequestContext({ fieldTypeMap: githubQueryFieldTypeMaps.simple }),
+          );
         });
 
-        requestData = getRequestData(githubParsedQueries.simple);
-
-        responseData = await cacheManager.resolveQuery(
-          requestData,
-          requestData,
-          githubQueryResponses.simple,
-          { awaitDataCaching: true },
-          getRequestContext({ fieldTypeMap: githubQueryFieldTypeMaps.simple }),
-        );
-      });
-
-      it("then the method should return the correct response data", () => {
-        expect(responseData).toMatchSnapshot();
-      });
-
-      it("then the cache should contain the correct data", async () => {
-        expect(await cacheManager.cache.export()).toMatchSnapshot();
-      });
-    });
-
-    describe("when caching is done through type cache directives", () => {
-      beforeAll(async () => {
-        cacheManager = await CacheManager.init({
-          cache: await Core.init({
-            name: "cachemap",
-            store: map(),
-          }),
-          typeCacheDirectives: {
-            Organization: "public, max-age=1",
-          },
-          typeIDKey: DEFAULT_TYPE_ID_KEY,
+        it("then the method should return the correct response data", () => {
+          expect(responseData).toMatchSnapshot();
         });
 
-        requestData = getRequestData(githubParsedQueries.simple);
-
-        responseData = await cacheManager.resolveQuery(
-          requestData,
-          requestData,
-          githubQueryResponses.simple,
-          { awaitDataCaching: true },
-          getRequestContext({ fieldTypeMap: githubQueryFieldTypeMaps.simple }),
-        );
+        it("then the cache should contain the correct data", async () => {
+          expect(await cacheManager.cache.export()).toMatchSnapshot();
+        });
       });
 
-      it("then the method should return the correct response data", () => {
-        expect(responseData).toMatchSnapshot();
-      });
+      describe("when caching is done through type cache directives", () => {
+        beforeAll(async () => {
+          cacheManager = await CacheManager.init({
+            cache: await Core.init({
+              name: "cachemap",
+              store: map(),
+            }),
+            typeCacheDirectives: {
+              Organization: "public, max-age=1",
+            },
+            typeIDKey: DEFAULT_TYPE_ID_KEY,
+          });
 
-      it("then the cache should contain the correct data", async () => {
-        expect(await cacheManager.cache.export()).toMatchSnapshot();
+          requestData = getRequestData(githubParsedQueries.simple);
+
+          responseData = await cacheManager.resolveQuery(
+            requestData,
+            requestData,
+            githubQueryResponses.simple,
+            { awaitDataCaching: true },
+            getRequestContext({ fieldTypeMap: githubQueryFieldTypeMaps.simple }),
+          );
+        });
+
+        it("then the method should return the correct response data", () => {
+          expect(responseData).toMatchSnapshot();
+        });
+
+        it("then the cache should contain the correct data", async () => {
+          expect(await cacheManager.cache.export()).toMatchSnapshot();
+        });
       });
     });
   });
