4864259659debce420ada7176c54a8fe4f227951
Dylan Aubrey <dylanaubrey@gmail.com>
2019-10-10 21:33:29 +0100

    chore: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ const repodogConfig = require('@repodog/jest-config');
 
 module.exports = {
   ...repodogConfig,
+  collectCoverageFrom: ['packages/**/*.ts', '!**/*.test.ts', '!**/bin/**', '!**/lib/**', '!**/node_modules/**'],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  testMatch: ['<rootDir>/packages/**/*.test.ts'],
 };
