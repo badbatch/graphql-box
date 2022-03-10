b5ddbc46f083ac60c2ed43cbf2f0e887601c34ba
Dylan Aubrey <dylanaubrey@gmail.com>
2022-03-10 22:30:53 +0000

    fix: add back eol

--- packages/gql.macro/src/helpers/writeFile.ts
+++ packages/gql.macro/src/helpers/writeFile.ts
@@ -2,6 +2,6 @@ import { appendFileSync } from "fs";
 
 export default (requestWhitelistPath: string, whitelist: string[]) => {
   whitelist.forEach(entry => {
-    appendFileSync(requestWhitelistPath, entry);
+    appendFileSync(requestWhitelistPath, `${entry}\n`);
   });
 };
