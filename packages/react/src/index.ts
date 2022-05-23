fae8a390c3c1d053183a925280fb57d54c980afd
Dylan Aubrey <dylanaubrey@gmail.com>
2022-05-23 23:36:41 +0100

    fix: expose type for connection listings component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,6 +3,7 @@ import useRequest from "./hooks/useRequest";
 export const useQuery = useRequest;
 export const useMutation = useRequest;
 export { default as ConnectionListings } from "./components/ConnectionListings";
+export { ListingsChildrenProps } from "./components/ConnectionListings/types";
 export * from "./contexts/GraphQLBox";
 export { default as useGraphQLBoxClient } from "./hooks/useGraphQLBoxClient";
 export { default as useSubscription } from "./hooks/useSubscription";
