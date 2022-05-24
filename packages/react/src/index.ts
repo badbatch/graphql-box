34eeb1de07da2e85c4f5ea4a1884d6849e300281
Dylan Aubrey <dylanaubrey@gmail.com>
2022-05-24 09:29:36 +0100

    fix: bug compiling code due to type import export issue

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,8 +2,8 @@ import useRequest from "./hooks/useRequest";
 
 export const useQuery = useRequest;
 export const useMutation = useRequest;
+export * from "./components/ConnectionListings/types";
 export { default as ConnectionListings } from "./components/ConnectionListings";
-export { ListingsChildrenProps } from "./components/ConnectionListings/types";
 export * from "./contexts/GraphQLBox";
 export { default as useGraphQLBoxClient } from "./hooks/useGraphQLBoxClient";
 export { default as useSubscription } from "./hooks/useSubscription";
