505b1895105a4e7f99035de01e4add021ca01ff7
Dylan Aubrey <dylanaubrey@gmail.com>
2022-05-25 23:27:56 +0100

    fix: connection resolver unsafe prop access bug

--- packages/connection-resolver/src/helpers/getStartAndEndCursors.ts
+++ packages/connection-resolver/src/helpers/getStartAndEndCursors.ts
@@ -1,8 +1,8 @@
 import { Edge } from "../defs";
 
-export const getStartCursor = (edges: Edge[]) => edges[0].cursor;
+export const getStartCursor = (edges: Edge[]) => edges[0]?.cursor;
 
 export const getEndCursor = (edges: Edge[]) => {
   const lastEdge = edges[edges.length - 1];
-  return lastEdge.cursor;
+  return lastEdge?.cursor;
 };
