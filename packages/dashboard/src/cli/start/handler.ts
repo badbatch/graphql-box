814ff2b3536193933a1efd3412af451a59b593ec
Dylan Aubrey <dylan.aubrey@bjss.com>
2024-04-27 17:31:48 +0100

    fix(dashboard): use npx to execute commands

--- packages/dashboard/src/cli/start/handler.ts
+++ packages/dashboard/src/cli/start/handler.ts
@@ -18,20 +18,24 @@ export const handler = (argv: StartHandlerArgv) => {
   const filePath = fileURLToPath(import.meta.url);
   const dashboardRootDir = resolve(filePath, '../../../');
 
-  shelljs.echo(colors.blue(`logTailPath: ${logTailPath}`));
-  shelljs.echo(colors.blue(`https: ${String(https)}`));
-  shelljs.echo(colors.blue(`moduleSystem: ${moduleSystem}`));
-  shelljs.echo(colors.blue(`cwd: ${process.cwd()}`));
-  shelljs.echo(colors.blue(`fullLogTailPath: ${fullLogTailPath}`));
-  shelljs.echo(colors.blue(`envVars: ${envVars}`));
-  shelljs.echo(colors.blue(`filePath: ${filePath}`));
-  shelljs.echo(colors.blue(`dashboardRootDir: ${dashboardRootDir}`));
+  shelljs.echo(colors.blue(`> logTailPath: ${logTailPath}`));
+  shelljs.echo(colors.blue(`> https: ${String(https)}`));
+  shelljs.echo(colors.blue(`> moduleSystem: ${moduleSystem}`));
+  shelljs.echo(colors.blue(`> cwd: ${process.cwd()}`));
+  shelljs.echo(colors.blue(`> fullLogTailPath: ${fullLogTailPath}`));
+  shelljs.echo(colors.blue(`> envVars: ${envVars}`));
+  shelljs.echo(colors.blue(`> filePath: ${filePath}`));
+  shelljs.echo(colors.blue(`> dashboardRootDir: ${dashboardRootDir}`));
+  shelljs.echo(`Changing working directory to ${dashboardRootDir}`);
 
   process.chdir(dashboardRootDir);
-  shelljs.echo(colors.blue(`cwd: ${process.cwd()}`));
+  shelljs.echo(colors.blue(`> new cwd: ${process.cwd()}`));
 
   try {
-    shelljs.exec(`${envVars} next build && rollup -c ./rollup.config.cjs && node dist/${moduleSystem}/server.mjs`);
+    shelljs.exec(
+      `${envVars} npx next build && npx rollup -c ./rollup.config.cjs && node dist/${moduleSystem}/server.mjs`
+    );
+
     return shelljs.exit(0);
   } catch (error: unknown) {
     shelljs.echo(colors.red(`Error: ${(error as Error).message}`));
