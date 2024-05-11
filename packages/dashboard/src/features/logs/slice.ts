8e80fe88be270295ed01cfd7751149372cd05b69
Dylan Aubrey <dylan.aubrey@bjss.com>
2024-05-11 07:54:09 +0100

    fix(dashboard): change way log entries are parsed on client

--- packages/dashboard/src/features/logs/slice.ts
+++ packages/dashboard/src/features/logs/slice.ts
@@ -1,6 +1,5 @@
 import { createAction, createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 import { encode } from 'js-base64';
-import { type Except } from 'type-fest';
 import { type LogEntry, type Store } from '../../types.ts';
 import { websocketMessage } from '../websocket/slice.ts';
 
@@ -9,12 +8,12 @@ const logsAdapter = createEntityAdapter<LogEntry>();
 export const logsSlice = createSlice({
   extraReducers: builder => {
     builder.addCase(websocketMessage, (state, action) => {
-      const parsed = (JSON.parse(action.payload) as string[]).map((message: string) => ({
-        ...(JSON.parse(message) as Except<LogEntry, 'id'>),
-        id: encode(message),
-      }));
+      const logEntry = {
+        ...JSON.parse(action.payload),
+        id: encode(action.payload),
+      } as LogEntry;
 
-      logsAdapter.setMany(state, parsed);
+      logsAdapter.setOne(state, logEntry);
     });
   },
   initialState: logsAdapter.getInitialState(),
