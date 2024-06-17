606b7bcd830d8a73be408bec61de274afacafdc1
Dylan Aubrey <dylan.aubrey@bjss.com>
2024-06-17 09:35:00 +0100

    refactor(react): support lazy initialisation of client

--- packages/react/src/contexts/GraphqlBox/Provider.tsx
+++ packages/react/src/contexts/GraphqlBox/Provider.tsx
@@ -1,13 +1,32 @@
 import { type Client } from '@graphql-box/client';
 import { type WorkerClient } from '@graphql-box/worker-client';
-import { type ReactNode } from 'react';
+import { type ReactNode, useEffect, useState } from 'react';
+import { type RequireAtLeastOne } from 'type-fest';
 import { Context } from './Context.ts';
 
-export type Props = {
+export type GraphqlBoxProviderProps = {
   children: ReactNode;
-  graphqlBoxClient: Client | WorkerClient;
+  /**
+   * Use instead of passing the client in on graphqlBoxClient
+   * if you need to lazy initialise the client because it is using
+   * browser APIs only available in client-only components.
+   */
+  createGraphqlBoxClient?: () => Client | WorkerClient;
+  graphqlBoxClient?: Client | WorkerClient;
 };
 
-export const Provider = ({ children, graphqlBoxClient }: Props) => (
-  <Context.Provider value={{ graphqlBoxClient }}>{children}</Context.Provider>
-);
+export const GraphqlBoxProvider = ({
+  children,
+  createGraphqlBoxClient,
+  graphqlBoxClient,
+}: RequireAtLeastOne<GraphqlBoxProviderProps, 'createGraphqlBoxClient' | 'graphqlBoxClient'>) => {
+  const [client, setClient] = useState<Client | WorkerClient | undefined>(graphqlBoxClient);
+
+  useEffect(() => {
+    if (createGraphqlBoxClient) {
+      setClient(createGraphqlBoxClient());
+    }
+  }, [createGraphqlBoxClient]);
+
+  return client ? <Context.Provider value={{ graphqlBoxClient: client }}>{children}</Context.Provider> : undefined;
+};
