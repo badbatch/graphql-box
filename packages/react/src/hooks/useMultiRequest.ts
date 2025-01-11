32e26f100856b652383841de840281ff73294aad
Dylan Aubrey <dylan.aubrey@bjss.com>
2025-01-11 22:41:35 +0000

    feat(react): add multi query and mutation hooks

---
+++ packages/react/src/hooks/useMultiRequest.ts
@@ -0,0 +1,53 @@
+import { type PartialRequestResult, type PlainObject, type RequestOptions } from '@graphql-box/core';
+import { useState } from 'react';
+import { useGraphqlBoxClient } from './useGraphqlBoxClient.ts';
+
+export type State<Data extends PlainObject> = {
+  data: Data[] | undefined;
+  errors: readonly Error[];
+  loading: boolean;
+};
+
+export const useMultiRequest = <Data extends PlainObject>(request: string, { loading = false } = {}) => {
+  const graphqlBoxClient = useGraphqlBoxClient();
+  const [state, setState] = useState<State<Data>>({ data: undefined, errors: [], loading });
+
+  const execute = async (optionsSet: RequestOptions[]) => {
+    setState({
+      data: undefined,
+      errors: [],
+      loading: true,
+    });
+
+    // This hook cannot be used with defer/stream so doesn't support
+    // returning an async iterator.
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const requestResults = (await Promise.all(
+      optionsSet.map(options => graphqlBoxClient.request(request, options)),
+    )) as PartialRequestResult[];
+
+    setState({
+      // @ts-expect-error Need to handle this properly
+      data: requestResults.reduce<Data[]>((acc, result) => {
+        if (result.data) {
+          return [...acc, result.data];
+        }
+
+        return acc;
+      }, []),
+      errors: requestResults.reduce<readonly Error[]>((acc, result) => {
+        if (result.errors?.length) {
+          return {
+            ...acc,
+            ...result.errors,
+          };
+        }
+
+        return acc;
+      }, []),
+      loading: false,
+    });
+  };
+
+  return { execute, ...state };
+};
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,5 +2,6 @@ export * from './components/ConnectionListings/index.tsx';
 export * from './components/ConnectionListings/types.ts';
 export * from './contexts/GraphqlBox/index.ts';
 export { useGraphqlBoxClient } from './hooks/useGraphqlBoxClient.ts';
+export { useMultiRequest as useMultiQuery, useMultiRequest as useMultiMutation } from './hooks/useMultiRequest.ts';
 export { useRequest as useQuery, useRequest as useMutation } from './hooks/useRequest.ts';
 export { useSubscription } from './hooks/useSubscription.ts';
