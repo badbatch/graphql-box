6cfe85231a61f4a0aaa40e469acb859080347df0
Dylan Aubrey <dylan.aubrey@bjss.com>
2025-02-11 23:47:08 +0000

    fix(react): create useQuery hook

---
+++ packages/react/src/hooks/useQuery.ts
@@ -0,0 +1,39 @@
+import { type PartialRequestResult, type PlainObject, type RequestOptions } from '@graphql-box/core';
+import { isAsyncIterable } from 'iterall';
+import { useState } from 'react';
+import { useGraphqlBoxClient } from './useGraphqlBoxClient.ts';
+
+export type State<Data extends PlainObject> = {
+  data: Data | null | undefined;
+  errors: readonly Error[];
+  loading: boolean;
+  requestID: string;
+};
+
+export const useQuery = <Data extends PlainObject>(request: string, { loading = false } = {}) => {
+  const graphqlBoxClient = useGraphqlBoxClient();
+  const [state, setState] = useState<State<Data>>({ data: undefined, errors: [], loading, requestID: '' });
+
+  const execute = async (options?: RequestOptions) => {
+    const requestResult = await graphqlBoxClient.query(request, options);
+
+    if (isAsyncIterable(requestResult)) {
+      throw new Error('useQuery does not support returning async iterators from queries');
+    }
+
+    // Need to use a type guard
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const { data, errors, requestID } = requestResult as PartialRequestResult;
+
+    setState({
+      // Need to fix the types here
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      data: data as Data,
+      errors: errors ?? [],
+      loading: false,
+      requestID,
+    });
+  };
+
+  return { execute, ...state };
+};
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,5 +3,6 @@ export * from './components/ConnectionListings/types.ts';
 export * from './contexts/GraphqlBox/index.ts';
 export { useGraphqlBoxClient } from './hooks/useGraphqlBoxClient.ts';
 export { useMultiRequest as useMultiQuery, useMultiRequest as useMultiMutation } from './hooks/useMultiRequest.ts';
-export { useRequest as useQuery, useRequest as useMutation } from './hooks/useRequest.ts';
+export { useQuery } from './hooks/useQuery.ts';
+export { useRequest as useMutation } from './hooks/useRequest.ts';
 export { useSubscription } from './hooks/useSubscription.ts';
