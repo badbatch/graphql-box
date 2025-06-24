c7aa8cc38a1917790a5f66e10ec702c3dd47b8e1
miami-man <badbatchdev@gmail.com>
2025-06-24 19:17:03 +0100

    refactor(server): remove excessive log data

--- packages/server/src/helpers/nextEnrichContextValue.ts
+++ packages/server/src/helpers/nextEnrichContextValue.ts
@@ -1,12 +1,10 @@
 import { type ServerRequestOptions } from '@graphql-box/core';
 import { type NextRequest } from 'next/server.js';
-import { UAParser } from 'ua-parser-js';
 
 export const nextEnrichContextValue = (req: NextRequest, options: ServerRequestOptions): ServerRequestOptions => {
   const headersList = req.headers;
   const cookieStore = req.cookies;
   const userAgent = headersList.get('user-agent') ?? undefined;
-  const { browser, cpu, device, engine, os } = UAParser(userAgent);
 
   return {
     ...options,
@@ -17,20 +15,10 @@ export const nextEnrichContextValue = (req: NextRequest, options: ServerRequestO
         apiRequestType: 'API_REQUEST',
         apiRouteHeaders: headersList,
         apiRouteUrl: req.nextUrl.href,
-        browserName: browser.name,
         browserPathname: headersList.get('x-browser-pathname'),
         browserUrl: headersList.get('x-browser-href'),
-        browserVersion: browser.version,
         cookies: cookieStore.getAll(),
-        cpuArchitecture: cpu.architecture,
-        deviceModel: device.model,
-        deviceType: device.type,
-        deviceVendor: device.vendor,
-        engineName: engine.name,
-        engineVersion: engine.version,
         ipAddress: headersList.get('x-real-ip') ?? headersList.get('x-forwarded-for'),
-        osName: os.name,
-        osVersion: os.version,
         referer: headersList.get('referer'),
         userAgent,
       },
