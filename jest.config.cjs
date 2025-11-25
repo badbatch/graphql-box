415ea6027cdb717da11979aa47e7573a4e96f971
miami-man <badbatchdev@gmail.com>
2025-11-25 23:05:00 +0000

    feat(root): add coverage generator

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,11 +8,16 @@ const jestConfig = require('@repodog/jest-config');
 const swcConfig = require('@repodog/swc-config');
 
 const isDebug = DEBUG === 'true';
+const config = jestConfig({ compilerOptions: swcConfig });
 
 module.exports = {
-  ...jestConfig({ compilerOptions: swcConfig }),
-  collectCoverage: false,
-  collectCoverageFrom: [],
+  ...config,
+  collectCoverageFrom: [
+    'packages/**/*.ts',
+    '!packages/core/**/*.ts',
+    '!packages/test-utils/**/*.ts',
+    ...config.collectCoverageFrom.slice(1),
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.mjs'],
   ...(isDebug ? {} : { testMatch: ['**/src/**/*.test.{ts,tsx}'] }),
 };
