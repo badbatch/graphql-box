be8d02d5bab140f82022cd3bb861adba4a87b900
miami-man <badbatchdev@gmail.com>
2026-01-26 15:13:03 +0000

    fix(fetch-manager): snapshot failing in ci

--- packages/fetch-manager/src/index.test.ts
+++ packages/fetch-manager/src/index.test.ts
@@ -19,6 +19,23 @@ Object.defineProperty(globalThis, 'location', {
 describe('@graphql-box/fetch-manager', () => {
   let fetchManager: RequestManagerDef;
 
+  beforeEach(() => {
+    // @ts-expect-error Okay for test file
+    globalThis.AbortController = class {
+      signal = {
+        aborted: false,
+        addEventListener: jest.fn(),
+        onabort: jest.fn(),
+        removeEventListener: jest.fn(),
+      };
+
+      abort() {
+        this.signal.aborted = true;
+        this.signal.onabort();
+      }
+    };
+  });
+
   describe('no batching', () => {
     describe('when batch is false', () => {
       let response: ResponseData;
@@ -121,14 +138,11 @@ describe('@graphql-box/fetch-manager', () => {
               "body": "{"batched":true,"operations":{"123456789":{"context":{"data":{"operationId":"123456789","operationName":"","operationType":"query","rawOperationHash":""}},"operation":"\\n  {\\n    organization(login: \\"facebook\\") {\\n      email\\n      login\\n      name\\n    }\\n  }\\n"}}}",
               "headers": Headers {},
               "method": "POST",
-              "signal": AbortSignal {
-                Symbol(kEvents): Map {},
-                Symbol(events.maxEventTargetListeners): 10,
-                Symbol(events.maxEventTargetListenersWarned): false,
-                Symbol(kHandlers): Map {},
-                Symbol(kAborted): false,
-                Symbol(kReason): undefined,
-                Symbol(kComposite): false,
+              "signal": {
+                "aborted": false,
+                "addEventListener": [MockFunction],
+                "onabort": [MockFunction],
+                "removeEventListener": [MockFunction],
               },
             },
           ]
@@ -200,14 +214,11 @@ describe('@graphql-box/fetch-manager', () => {
               "body": "{"batched":true,"operations":{"123456789":{"context":{"data":{"operationId":"123456789","operationName":"","operationType":"query","rawOperationHash":""}},"operation":"\\n  {\\n    organization(login: \\"facebook\\") {\\n      email\\n      login\\n      name\\n    }\\n  }\\n"},"0123456878":{"context":{"data":{"operationId":"0123456878","operationName":"","operationType":"query","rawOperationHash":""}},"operation":"\\n  {\\n    organization(login: \\"google\\") {\\n      email\\n      login\\n      name\\n    }\\n  }\\n"}}}",
               "headers": Headers {},
               "method": "POST",
-              "signal": AbortSignal {
-                Symbol(kEvents): Map {},
-                Symbol(events.maxEventTargetListeners): 10,
-                Symbol(events.maxEventTargetListenersWarned): false,
-                Symbol(kHandlers): Map {},
-                Symbol(kAborted): false,
-                Symbol(kReason): undefined,
-                Symbol(kComposite): false,
+              "signal": {
+                "aborted": false,
+                "addEventListener": [MockFunction],
+                "onabort": [MockFunction],
+                "removeEventListener": [MockFunction],
               },
             },
           ]
